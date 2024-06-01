617100ba4d45748a58239ab9a1af1b68ff8babf1
h1ne <henryharlay@gmail.com>
2024-06-01 21:14:31 +0900

    Add

---
+++ aprob/a356.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class a356 {
+  public static void main() {
+    Scanner getval = new Scanner(System.in);
+    int N = getval.nextInt();
+    int L = getval.nextInt();
+    int R = getval.nextInt();
+
+  }
+}

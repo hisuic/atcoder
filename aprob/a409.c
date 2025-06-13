c778ce854a7d584e1007d73cba4fa9afdab67120
hisui <henryharlay@gmail.com>
2025-06-13 20:48:35 +0900

    a

---
+++ aprob/a409.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+  int N;
+  scanf("%d", &N);
+
+  char T[N];
+  scanf("%s", T);
+  char A[N];
+  scanf("%s", A);
+
+  for (int i = 0; i < N; i++) {
+    if (T[i] == 'o' && A[i] == 'o') {
+      printf("Yes");
+      return 0;
+    }
+  }
+  printf("No");
+  return 0;
+}
